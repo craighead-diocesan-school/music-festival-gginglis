cdc3b188a2f3d1b6252e1549c7ffce49edf5b07e
gginglis <inglisge23@craighead.school.nz>
2024-05-28 14:05:04 +1200

    footer initial
    styled footer component with colour and fonts, checked in chrome

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,15 @@
 <footer>
-    <p>&copy; Craighead Diocesan School 2024</p>
+    <p>&copy; Georgiana Inglis 2024</p>
 </footer>
 
 <style>
+    footer {
+        color: #9468cf;
+        font-size: 1.25rem;
+        font-family: "KoHo", sans-serif;
+        font-weight: 500;
+        background-color: #f6e9ff;
+        text-align: center;
+        padding: 20px;
+    }
 </style>
